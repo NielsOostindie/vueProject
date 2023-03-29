cb1e8622d52475a4b2de214e22cda5ff7e4645df
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2023-03-29 13:48:34 +0200

    add a Button with a on click and a container

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="onClick()" :style="{background: color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+import { stringifyExpression } from '@vue/compiler-core';
+
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick(){
+            console.log('click')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
